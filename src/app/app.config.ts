1e7363146ca0564087777d571ec0e679330e7d02
theloyal1 <106931588+theloyal1@users.noreply.github.com>
2026-05-28 22:19:46 -0300

    feat: create login page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import('@angular/platform-browser/animations').then(m => m.provideAnimations());
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideToastr(), provideAnimations(), provideHttpClient(withFetch())]
 };
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,22 +2,37 @@ import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [DefaultLoginLayoutComponent, ReactiveFormsModule, PrimaryInputComponent],
+  providers: [LoginService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginForm!: FormGroup;
 
-  constructor() {
+  constructor(private router: Router, private loginService: LoginService, private toastService: ToastrService) {
     this.loginForm = new FormGroup({
       // Defina os controles do formulário aqui, por exemplo:
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
     });
+  }
+
+  submit() {
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => this.toastService.success('Login realizado com sucesso!'),
+      error: (err) => this.toastService.error('Erro ao realizar login: ' + err.message)
+    });
+  }
+
+  navigate() {
+    this.router.navigate(['/cadastro']);
   }
 }
